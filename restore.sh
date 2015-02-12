10a5d5c9f5a76075294fddeb94d99cc91ab2ec62
Tomoki Odaka <odaka_to@worksap.co.jp>
2015-02-12 22:17:36 +0900

    fix 1 liner for find latest backup
    current script do not work when "aws s3 ls" shows such as below.
    2015-02-12 17:42:28  492667043 cassandra.2015-02-12-08-39-12.tar.gz
    2015-02-12 21:52:44        328 elasticsearch.2015-02-12-12-52-39.tar.gz

--- restore.sh
+++ restore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Find last backup file
-LAST_BACKUP=$(aws s3 ls s3://$S3_BUCKET_NAME | cut -d ' ' -f5 | grep ^$BACKUP_NAME | sort -r | head -n1)
+LAST_BACKUP=$(aws s3 ls s3://$S3_BUCKET_NAME | awk -F " " '{print $4}' | grep ^$BACKUP_NAME | sort -r | head -n1)
 
 # Download backup from S3
 aws s3 cp s3://$S3_BUCKET_NAME/$LAST_BACKUP $LAST_BACKUP
